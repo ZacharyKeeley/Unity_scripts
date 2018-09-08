90b02d52fa6a41edca4bb00885e48b87570df52a
ZacharyKeeley <keeleyz3929@yahoo.com>
2018-09-08 03:00:14 -0400

    Add files via upload

---
+++ score.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+// this allows us to use 
+using UnityEngine.UI;
+
+public class score : MonoBehaviour {
+
+    // this references the transform object in the player instance
+    public Transform player;
+
+    // we create a reference to our text object
+    public Text scoreText;
+	
+	// Update is called once per frame
+	void Update () {
+
+        // we assign the player's z position to the text part of the UI
+        // we must also convert the float to a string 
+        // this is done by using the ToString() method
+        scoreText.text = player.position.z.ToString("0");
+
+        // uncomment the next line to initialize debugging of player position
+        // Debug.log(player.position.z);
+	}
+}
