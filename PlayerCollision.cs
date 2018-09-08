69c123ce5fd9c8c20527bf2dc8be77c98b2b7e84
ZacharyKeeley <keeleyz3929@yahoo.com>
2018-09-08 02:35:24 -0400

    Add files via upload

---
+++ PlayerCollision.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+    
+    // we create a public reference to our player movement script called movement
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacle")
+        {
+            // Debug.Log("We hit an Obstacle.");
+            // this causes the player to stop moving if he hits an obstacle
+            movement.enabled = false;
+        }
+        Debug.Log(collisionInfo.collider.name);
+    }
+}
---
+++ PlayerMovement.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public Rigidbody rb;
+
+    // Use this for initialization
+    // void Start () {
+    // Debug.Log("Hello World");
+    //  rb.AddForce(0, 200, 500);
+    //}
+
+    public float forwardForce = 2000f;
+    public float sidewaysForce = 500f;
+
+
+	// Fixed update is used to deal with physics
+	void FixedUpdate ()
+    {
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+
+
+
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
+        }
+    }
+}
